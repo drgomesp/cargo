3df285dec4cf74fd6da55b0027b701e67e44882f
drgomesp <drgomesp@gmail.com>
2017-06-02 23:28:03 +0400

    Replaced glide with dep

--- container/interface.go
+++ container/interface.go
@@ -7,4 +7,5 @@ type Interface interface {
 	Register(id string, arg interface{}) (def *definition.Definition, err error)
 	Set(id string, arg interface{}) (service *interface{}, err error)
 	Get(id string) (service *interface{}, err error)
+	MustGet(id string) interface{}
 }
